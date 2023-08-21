f775ad04a0bd35cad977f74f196fbfcbc29f023f
sovann-kai <sovanns25b@gmail.com>
2023-08-21 23:49:42 +0700

    sovann

--- bot.js
+++ bot.js
@@ -148,6 +148,7 @@ bot.action(/service_(.+)_(.+)/, async (ctx) => {
                             \nTelegram: ${phone.telegram}
                             \n${locationText}
                         `;
+                        await ctx.replyWithPhoto({ source: posterurl, caption: message });
                         await ctx.replyWithHTML(message);
                         await ctx.replyWithHTML("ព័ត៌មានបន្ថែមពីយើងខ្ញុំ: <a href='https://t.me/sdaudigital'>Link</a>")
                     } else {
